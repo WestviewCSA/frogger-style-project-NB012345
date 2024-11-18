b4c4a91b21da7db3a0bd19c1a39c224a705ba5ff
1937192 <1937192@01-18115.pusd.dom>
2024-11-18 11:23:24 -0800

    almost finished

--- FroggerStyleProject/src/Frame.java
+++ FroggerStyleProject/src/Frame.java
@@ -22,6 +22,9 @@ import javax.swing.Timer;
 
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
 	
+	//for any debugging code we add
+	public static boolean debugging = true;
+	
 	//Timer related variables
 	int waveTimer = 5; //each wave of enemies is 20s
 	long ellapseTime = 0;
@@ -34,15 +37,163 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 //	Music soundBang = new Music("bang.wav", false);
 //	Music soundHaha = new Music("haha.wav", false);
 	
+	//stays at the top so all other objects go on the bottom
+	Background theBackground = new Background(0,0);
+	
+	penguin myPenguin = new penguin(250, 670);
+	Present present1 = new Present(250, 25);
+	
+	//penguin myPenguin2 = new penguin(100, 200);
+	
+	//a row of penguinScrolling objects
+	PenguinScroller[] row1 = new PenguinScroller[10];
+	
 	//frame width/height
 	int width = 600;
-	int height = 600;	
+	int height = 800;	
 	
-
+	IceBlock myIce = new IceBlock(200, 400);
+	IceScroller[] row2 = new IceScroller[5];
+	IcedScroller[] row2i =  new IcedScroller[4];
+	
+	//JellyFish myJellyFish = new JellyFish(50,50);
+	JellyfishScroller[] row3 = new JellyfishScroller[4];
+	
+	//Polarbear myPolarBear = new Polarbear(10,10);
+	PolarBearScroller[] row4 = new PolarBearScroller[4];
+	
+	PolarBearOtherScroller[] row4i = new PolarBearOtherScroller[4];
+	//Seal mySeal = new Seal(200,200);
+	SealScroller[] row5 = new SealScroller[4];
+	
+	DeerScroller[] row6 = new DeerScroller[4];
 	public void paint(Graphics g) {
 		super.paintComponent(g);
+		
+		theBackground.paint(g);
+		present1.paint(g);
+		
+		//these two need to be here because the penguin goes on top of these objects
+		
+		boolean riding = false;
+		for(IcedScroller obj : row2i) {
+			obj.paint(g);
+			
+		}
+		for(IcedScroller obj : row2i) {
+			if(obj.collided(myPenguin)) {
+				myPenguin.setvx(obj.getvx());
+				riding = true;
+				break;
+			}
+		}
+		for(IceScroller obj : row2) {
+			if(obj.collided(myPenguin)) {
+				myPenguin.setvx(obj.getvx());
+				riding = true;
+				break;
+			}
+		}
+		//main character stops moving if not on a rideable object
+		//but also lets limit it in the y
+		
+		if((!riding && myPenguin.getY() == 370) || (!riding && myPenguin.getY() == 265)) {//|| (!riding && myPenguin.getY() < 380 && !riding && myPenguin.getY() < 420)) {
+			riding = false;
+			myPenguin.setvx(0);
+			myPenguin.x = 250;
+			myPenguin.y = 670;
+			//if i riding any the duck is in the water area
+			//reset back to starting 
+			System.out.println("you fell into the water");
 
+		}
+		else if (!riding ) {
+			myPenguin.setvx(0);
+			//System.out.println("you fell into the water");
+
+		}
+		
+		
+		for(IceScroller obj : row2) {
+			obj.paint(g);
+		}
+		//paint the other objects on the screen
+		myPenguin.paint(g);
+
+		
+
+		//have the row 1 objects paint on the screen
+		//or each obj in row 
+		
+		
+		
+		
+		
+		//jelly fish
+		//myJellyFish.paint(g);
+		for(JellyfishScroller obj : row3) {
+			obj.paint(g);
+			if(obj.collided(myPenguin)) {
+				System.out.println("ouch");
+				myPenguin.setY(670);
+				myPenguin.setX(250);
+			}
+		}
+		
+		//polar bear obj
+		//myPolarBear.paint(g);
+		for(PolarBearScroller obj : row4) {
+			obj.paint(g);
+			if(obj.collided(myPenguin)) {
+				System.out.println("ouch");
+				myPenguin.setY(670);
+				myPenguin.setX(250);
+			}
+		}
+		
+		
+		for(PolarBearOtherScroller obj : row4i) {
+			obj.paint(g);
+			if(obj.collided(myPenguin)) {
+				System.out.println("ouch");
+				myPenguin.setY(670);
+				myPenguin.setX(250);
+			}
+		}
+		//mySeal.paint(g);
+		for(SealScroller obj : row5) {
+			obj.paint(g);
+			if(obj.collided(myPenguin)) {
+				System.out.println("ouch");
+				myPenguin.setY(670);
+				myPenguin.setX(250);
+			}
+		}
+		
+		for(DeerScroller obj : row6) {
+			obj.paint(g);
+			if(obj.collided(myPenguin)) {
+				System.out.println("ouch");
+				myPenguin.setY(670);
+				myPenguin.setX(250);
+			}
+		}
+		
+		if(present1.collided(myPenguin)) {
+			System.out.println("YAY!");
+			myPenguin.setY(670);
+			myPenguin.setX(250);
+			System.out.println("Game Reset");
+
+		}
 	}
+	
+	//collision detection
+	//for each ghost object in row array
+	
+	
+	
+	
 	
 	public static void main(String[] arg) {
 		Frame f = new Frame();
@@ -58,10 +209,40 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
  		f.addMouseListener(this);
 		f.addKeyListener(this);
 	
-		backgroundMusic.play();
+//		backgroundMusic.play();
 
-	
+		// setup any 1d array here - create objects that go in them;l
+	// traverse the array
+		for(int i = 0; i < row1.length; i++) {
+			row1[i] = new PenguinScroller(i*200,300); 
+		}
 		
+		for( int j = 0; j <row2.length; j++) {
+			row2[j] = new IceScroller(j*200, 200);
+		}
+		
+		for(int y =0; y < row2i.length; y++) {
+			row2i[y] = new IcedScroller(y*200, 345);
+		}
+		
+		for(int k = 0; k< row3.length; k++ ) {
+			row3[k] = new JellyfishScroller(k*200,270);
+		}
+		
+		for(int n = 0; n< row4.length; n++) {
+			row4[n] = new PolarBearScroller(n*200, 450);
+		}
+		
+		for(int r = 0; r <row4i.length; r++) {
+			row4i[r] = new PolarBearOtherScroller(r*200, 570);
+		}
+		for(int p =0; p < row5.length; p++) {
+			row5[p] = new SealScroller(p*200, 140);
+		}
+		
+		for (int e =0; e <row6.length; e++) {
+			row6[e] = new DeerScroller(e*250, 500);
+		}
 		//the cursor image must be outside of the src folder
 		//you will need to import a couple of classes to make it fully 
 		//functional! use eclipse quick-fixes
@@ -80,7 +261,7 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -118,7 +299,19 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		System.out.println(arg0.getKeyCode());
+		if(arg0.getKeyCode() ==87) {
+			//move character up
+			myPenguin.move(0);
+		}else if (arg0.getKeyCode() ==83) {
+			 myPenguin.move(1);
+			//move character down
+		}
 		
+		if(arg0.getKeyCode() ==65) {
+			myPenguin.move(2);
+		}else if (arg0.getKeyCode() ==68) {
+			myPenguin.move(3);
+		}
 		
 		
 	}
